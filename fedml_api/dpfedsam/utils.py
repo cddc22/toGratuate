9d536e8ce37622c54e6629d9e9db90427f43601e
d <cddc22@users.noreply.github.com>
2024-11-11 21:13:40 +0800

    Update utils.py

--- fedml_api/dpfedsam/utils.py
+++ fedml_api/dpfedsam/utils.py
@@ -1,11 +1,11 @@
 import contextlib
 
 import torch.nn as nn
-
+from torch.nn.modules.batchnorm import _BatchNorm
 
 def disable_running_stats(model):
     def _disable(module):
-        if isinstance(module, nn.BatchNorm2d):
+        if isinstance(module, _BatchNorm):
             module.backup_momentum = module.momentum
             module.momentum = 0
 
@@ -14,7 +14,7 @@ def disable_running_stats(model):
 
 def enable_running_stats(model):
     def _enable(module):
-        if isinstance(module, nn.BatchNorm2d) and hasattr(module, "backup_momentum"):
+        if isinstance(module, _BatchNorm) and hasattr(module, "backup_momentum"):
             module.momentum = module.backup_momentum
 
     model.apply(_enable)

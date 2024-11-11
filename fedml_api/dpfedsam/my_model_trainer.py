0d7a83e736c5e1baf8ba75b402bd53eb4929a2fb
d <cddc22@users.noreply.github.com>
2024-11-11 20:59:46 +0800

    Update my_model_trainer.py

--- fedml_api/dpfedsam/my_model_trainer.py
+++ fedml_api/dpfedsam/my_model_trainer.py
@@ -50,10 +50,13 @@ class MyModelTrainer(ModelTrainer):
         # 损失函数
         criterion = nn.CrossEntropyLoss().to(device)
 
+        if args.client_optimizer == "sgd":
+            optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, self.model.parameters()), lr=args.lr* (args.lr_decay**round), momentum=args.momentum,weight_decay=args.wd)
+
         # 初始化 WeightedSAM 优化器
         base_optimizer = torch.optim.SGD
         optimizer = WeightedSAM(
-            model=model,
+            model=model.parameters(),
             base_optimizer=base_optimizer(filter(lambda p: p.requires_grad, self.model.parameters()), lr=args.lr * (
                     args.lr_decay ** round) * 0.1, momentum=args.momentum, weight_decay=args.wd),
             rho=args.rho,
